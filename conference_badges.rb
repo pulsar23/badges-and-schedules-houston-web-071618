12a363a722fe7e5d2438b969e7709cb103e0cb8e
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-10 08:07:59 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_array)
+  array_msg = []
+  name_array.each do |name|
+    new_msg = badge_maker(name)
+    array_msg.push(new_msg)
+  end
+  return array_msg
+end
+
+def assign_rooms(name_array)
+  array_rm_msg = []
+  x = (1..7).to_a 
+  name_array.each_with_index do |name, idx|
+    array_rm_msg = "Hello, #{name}! You'll be assigned to room #{x[idx]}!"
+  end
+  return array_rm_msg
+end
